b0de83d0d9c58c0a0d7451a937ba3e6921d81ea6
Vegar Norman <vegar.norman@kristiania.no>
2021-01-17 13:57:42 +0100

    Containers should get data from Cosmic

--- src/containers/ContactContainer/index.jsx
+++ src/containers/ContactContainer/index.jsx
@@ -1,9 +1,48 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import Cosmic from 'cosmicjs';
 
 function ContactContainer() {
+  const [pageData, setPageData] = useState(null);
+
+  useEffect(() => {
+    const client = new Cosmic();
+    const bucket = client.bucket({
+      slug: process.env.BUCKET_SLUG,
+      read_key: process.env.READ_KEY
+    });
+
+    bucket.getObject({
+      slug: 'kontakt-meg', // Merk! Denne slugen er bare et eksempel - endre den!
+      props: 'title,content'
+    })
+      .then(data => {
+        setPageData(data.object);
+      })
+      .catch(error => {
+        console.error(error);
+      });
+  }, []);
+
+  function renderSkeleton() {
+    return (
+      <p>Laster data...</p>
+    );
+  }
+
+  function renderPage() {
+    return (
+      <main>
+        <h1>{pageData.title}</h1>
+        <div dangerouslySetInnerHTML={{__html: pageData.content}} />
+      </main>
+    )
+  }
+
   return (
-    <h1>Kontakt</h1>
-  );
-}
+    <>
+      {(pageData === null) ? renderSkeleton() : renderPage()}
+    </>
+  )
+};
 
 export default ContactContainer;
