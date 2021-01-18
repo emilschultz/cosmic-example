cafc405e5e28bd99c04482a129c2209c228cfa02
Vegar Norman <vegar.norman@kristiania.no>
2021-01-18 14:05:51 +0100

    Debugging

--- webpack.config.prod.js
+++ webpack.config.prod.js
@@ -4,6 +4,8 @@ const { CleanWebpackPlugin } = require('clean-webpack-plugin');
 const webpack = require('webpack');
 
 module.exports = env => {
+  console.log(env);
+  
   return {
     mode: 'production',
     entry: {
