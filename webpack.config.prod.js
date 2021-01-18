e6d7a81c7dd15f11f144bf99b57b770f9e111a67
Vegar Norman <vegar.norman@kristiania.no>
2021-01-18 13:25:09 +0100

    Changing prod config

--- webpack.config.prod.js
+++ webpack.config.prod.js
@@ -4,47 +4,51 @@ const { CleanWebpackPlugin } = require('clean-webpack-plugin');
 const webpack = require('webpack');
 const Dotenv = require('dotenv-webpack');
 
-module.exports = {
-  mode: 'production',
-  entry: {
-    index: './src/index.js'
-  },
-  devtool: 'source-map',
-  plugins: [
-    new Dotenv({
-      path: './.production.env'
-    }),
-    new CleanWebpackPlugin(),
-    new HtmlWebpackPlugin({
-      filename: 'index.html',
-      template: './src/template.html',
-      title: 'Produksjon'
-    }),
-    new webpack.HotModuleReplacementPlugin()
-  ],
-  output: {
-    filename: '[name].bundle.js',
-    path: path.resolve(__dirname, 'build')
-  },
-  module: {
-    rules: [
-      {
-        test: /\.(js|jsx)$/,
-        exclude: /node_modules/,
-        use: {
-          loader: 'babel-loader',
-          options: {
-            plugins: ['@babel/plugin-transform-runtime']
-          }
-        }
-      },
-      {
-        test: /\.(png|svg|jpg|jpeg|gif)$/i,
-        type: 'asset/resource',
-      }
+module.exports = env => {
+  console.log(env);
+  
+  return {
+    mode: 'production',
+    entry: {
+      index: './src/index.js'
+    },
+    devtool: 'source-map',
+    plugins: [
+      new Dotenv({
+        path: './.production.env'
+      }),
+      new CleanWebpackPlugin(),
+      new HtmlWebpackPlugin({
+        filename: 'index.html',
+        template: './src/template.html',
+        title: 'Produksjon'
+      }),
+      new webpack.HotModuleReplacementPlugin()
     ],
-  },
-  resolve: {
-    extensions: ['*', '.js', '.jsx']
-  }
+    output: {
+      filename: '[name].bundle.js',
+      path: path.resolve(__dirname, 'build')
+    },
+    module: {
+      rules: [
+        {
+          test: /\.(js|jsx)$/,
+          exclude: /node_modules/,
+          use: {
+            loader: 'babel-loader',
+            options: {
+              plugins: ['@babel/plugin-transform-runtime']
+            }
+          }
+        },
+        {
+          test: /\.(png|svg|jpg|jpeg|gif)$/i,
+          type: 'asset/resource',
+        }
+      ],
+    },
+    resolve: {
+      extensions: ['*', '.js', '.jsx']
+    }
+  };
 };
