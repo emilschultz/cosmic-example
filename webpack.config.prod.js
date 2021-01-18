e56470c66aae171833c00fdc68bed4f76cf48d64
Vegar Norman <vegar.norman@kristiania.no>
2021-01-18 14:40:38 +0100

    Set up CopyPlugin to copy in _redirects

--- webpack.config.prod.js
+++ webpack.config.prod.js
@@ -24,8 +24,8 @@ module.exports = () => {
       }),
       new CopyPlugin({
         patterns: [
-          {from: "other", to: "build"}
-        ]
+          {from: './other'}
+        ],
       })
     ],
     output: {
